a56272812a1075578b4579d120b0f4cb0fb64d86
Kengo TODA <skypencil@gmail.com>
2019-08-06 12:58:53 +0800

    chore: format code by spotless

--- src/main/java/com/github/spotbugs/cansada/nullness/Nullness.java
+++ src/main/java/com/github/spotbugs/cansada/nullness/Nullness.java
@@ -1,41 +1,58 @@
+/*
+ * Copyright (c) 2019-present The SpotBugs team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.spotbugs.cansada.nullness;
 
 import codeanalysis.experimental.annotations.Nullable;
 
 enum Nullness {
-    UNKNOWN(true), NULLABLE(true), NOT_NULL(false);
+  UNKNOWN(true),
+  NULLABLE(true),
+  NOT_NULL(false);
 
-    private final boolean canBeNull;
+  private final boolean canBeNull;
 
-    Nullness(boolean canBeNull) {
-        this.canBeNull = canBeNull;
+  Nullness(boolean canBeNull) {
+    this.canBeNull = canBeNull;
+  }
+
+  boolean canBeNull() {
+    return this.canBeNull;
+  }
+
+  @Nullable
+  public static Nullness from(@Nullable String descriptor) {
+    if (descriptor == null) {
+      return null;
     }
 
-    boolean canBeNull() {
-        return this.canBeNull;
+    switch (descriptor) {
+      case "Lcodeanalysis/experimental/annotations/NullnessUnknown;":
+      case "codeanalysis/experimental/annotations/NullnessUnknown":
+      case "codeanalysis.experimental.annotations.NullnessUnknown":
+        return UNKNOWN;
+      case "Lcodeanalysis/experimental/annotations/Nullable;":
+      case "codeanalysis/experimental/annotations/Nullable":
+      case "codeanalysis.experimental.annotations.Nullable":
+        return NULLABLE;
+      case "Lcodeanalysis/experimental/annotations/NotNull;":
+      case "codeanalysis/experimental/annotations/NotNull":
+      case "codeanalysis.experimental.annotations.NotNull":
+        return NOT_NULL;
+      default:
+        return null;
     }
-
-    @Nullable
-    public static Nullness from(@Nullable String descriptor) {
-        if (descriptor == null) {
-            return null;
-        }
-
-        switch (descriptor) {
-        case "Lcodeanalysis/experimental/annotations/NullnessUnknown;":
-        case "codeanalysis/experimental/annotations/NullnessUnknown":
-        case "codeanalysis.experimental.annotations.NullnessUnknown":
-            return UNKNOWN;
-        case "Lcodeanalysis/experimental/annotations/Nullable;":
-        case "codeanalysis/experimental/annotations/Nullable":
-        case "codeanalysis.experimental.annotations.Nullable":
-            return NULLABLE;
-        case "Lcodeanalysis/experimental/annotations/NotNull;":
-        case "codeanalysis/experimental/annotations/NotNull":
-        case "codeanalysis.experimental.annotations.NotNull":
-            return NOT_NULL;
-        default:
-            return null;
-        }
-    }
+  }
 }
