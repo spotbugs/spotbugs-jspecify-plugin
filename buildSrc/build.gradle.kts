0b28ff36cf9cfb7b3e69f1cf9462985c051b2bd9
Kengo TODA <skypencil@gmail.com>
2021-08-29 20:45:04 +0800

    build: bump up spotless-plugin-gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.14.2")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.14.3")
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:2.0.2")
     implementation("org.javamodularity:moduleplugin:1.8.7")
 }
